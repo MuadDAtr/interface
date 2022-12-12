8ab665e7612406f60cdcb0bd087eb3f89ab09c50
MuadDAtr <int.stron@protonmail.com>
2022-12-12 23:37:49 +0100

    adding label

--- gui_inter.py
+++ gui_inter.py
@@ -4,4 +4,6 @@ import tkinter as tk
 window = tk.Tk()
 window.geometry("1000x600")
 window.title("My GUI interface")
+label = tk.Label(window,text = "My GUI", font = ("Times New Roman", 24))
+label.pack()
 window.mainloop()
