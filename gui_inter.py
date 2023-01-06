8f5e849e14605743ea9aa904215167de403e33a9
MuadDAtr <int.stron@protonmail.com>
2023-01-06 23:36:54 +0100

    buttn

--- gui_inter.py
+++ gui_inter.py
@@ -9,4 +9,8 @@ label.pack(padx=35, pady=35)
 
 textbox = tk.Text(window, font=('Times New Roman', 30), height = 5)
 textbox.pack(padx=40, pady=40)
+
+button = tk.Button(window, text = "Log in", font = ('Vedrana', 14))
+button.pack()
+
 window.mainloop()
