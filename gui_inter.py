521569134272d5c3be06ad1ff4f59c4311d4a9d2
MuadDAtr <int.stron@protonmail.com>
2023-01-07 22:51:43 +0100

    adding buttons

--- gui_inter.py
+++ gui_inter.py
@@ -15,5 +15,17 @@ button.pack()
 
 bframe = tk.Frame(window)
 bframe.columnconfigure(0, weight=1)
+bframe.columnconfigure(1, weight=1)
+
+button1 = tk.Button(bframe, text = "Przycisk 1", font =("Vedrana", 14))
+button1.grid(row = 0, column = 0)
+button2 = tk.Button(bframe, text = "Przycisk 2", font =("Vedrana", 14))
+button2.grid(row = 0, column = 1)
+button3 = tk.Button(bframe, text = "Przycisk 3", font =("Vedrana", 14))
+button3.grid(row = 1, column = 0)
+button3 = tk.Button(bframe, text = "Przycisk 4", font =("Vedrana", 14))
+button3.grid(row = 1, column = 1)
+
+bframe.pack()
 
 window.mainloop()
