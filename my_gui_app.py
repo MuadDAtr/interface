17565d2901d42631327f6af00397b89803fb899a
MuadDAtr <int.stron@protonmail.com>
2023-01-08 23:50:39 +0100

    my_gui

--- my_gui_app.py
+++ my_gui_app.py
@@ -3,4 +3,9 @@ import tkinter as tk
 
 class MyGUI:
     def __init__(self):
-        self.
+        self.root = tk.Tk()
+
+        self.label = tk.Label(self.root, text = "Hello, Hi!", font = ("Vedrana", 20))
+        self.label.pack(padx = 30, pady = 50)
+        self.textbox = tk.Textbox()
+        self.root.mainloop()
