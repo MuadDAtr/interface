5628099fa71f1fd6d2d9ba45981c44fa9c68d47e
MuadDAtr <int.stron@protonmail.com>
2023-01-08 23:59:42 +0100

    check

--- my_gui_app.py
+++ my_gui_app.py
@@ -9,7 +9,10 @@ class MyGUI:
         self.label.pack(padx = 30, pady = 50)
         self.textbox = tk.Textbox(self.root, font = ('Times New Roman', 13))
         self.textbox.pack(padx = 20, pady = 10)
- 
-        self.check = tk.Checkbutton(self.root, text = "Testing", font = ('Arial', 17))
-        self.check.pacj()
+
+        self.ch_status = tk.IntVar()
+        self.check = tk.Checkbutton(self.root, text = "Testing", font = ('Arial', 17), variable = self.ch_status)
+        self.check.pack(padx = 20, pady = 20)
+
+
         self.root.mainloop()
