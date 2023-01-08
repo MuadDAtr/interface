ea98034e11dd5a400914566deb1fc3366589b6d3
MuadDAtr <int.stron@protonmail.com>
2023-01-09 00:07:08 +0100

    executing class

--- my_gui_app.py
+++ my_gui_app.py
@@ -7,12 +7,19 @@ class MyGUI:
 
         self.label = tk.Label(self.root, text = "Hello, Hi!", font = ("Vedrana", 20))
         self.label.pack(padx = 30, pady = 50)
-        self.textbox = tk.Textbox(self.root, font = ('Times New Roman', 13))
+        self.textbox = tk.Text(self.root, font = ('Times New Roman', 13))
         self.textbox.pack(padx = 20, pady = 10)
 
         self.ch_status = tk.IntVar()
         self.check = tk.Checkbutton(self.root, text = "Testing", font = ('Arial', 17), variable = self.ch_status)
         self.check.pack(padx = 20, pady = 20)
 
+        self.button = tk.Button(self.root, text = "I love you", font = ('Baskerville', 24), command = self.test_it)
+        self.button.pack(padx = 20, pady = 10)
 
         self.root.mainloop()
+    
+    def test_it(self):
+        pass
+
+MyGUI()
