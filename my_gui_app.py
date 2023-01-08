b274f2e6eacc437def906bde2da9f8c3667e2009
MuadDAtr <int.stron@protonmail.com>
2023-01-08 23:53:51 +0100

    check

--- my_gui_app.py
+++ my_gui_app.py
@@ -7,5 +7,9 @@ class MyGUI:
 
         self.label = tk.Label(self.root, text = "Hello, Hi!", font = ("Vedrana", 20))
         self.label.pack(padx = 30, pady = 50)
-        self.textbox = tk.Textbox()
+        self.textbox = tk.Textbox(self.root, font = ('Times New Roman', 13))
+        self.textbox.pack(padx = 20, pady = 10)
+
+        self.check
+
         self.root.mainloop()
